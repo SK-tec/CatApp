a0b1ff9c50d545b4ba79a5ec7df10ac8fd29234a
David Niermann <dc.niermann@gmail.com>
2023-03-09 16:17:10 +0100

    componentsDependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import SearchResults from "./components/SearchResults";
+import Cats from "./components/Cats";
+import CatDetails from "./components/CatDetails.js";
+import Footer from "./components/Footer";
+import CreateEditCat from "./components/CreateEditCat";
 
 function App() {
   return (
     <div className="App">
-    <h1>Welcome to CAT App</h1>
+      <Header />
+      <SearchBar />
+      <Routes>
+        <Route path="/" element={<Cats />} />
+        <Route path="/search/:query" element={<SearchResults />} />
+        <Route path="/cats/:id" element={<CatDetails />} />
+        <Route path="/createEditCat" element={<CreateEditCat />} />
+        <Route path="/breed/:breed" element={<Cats />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
