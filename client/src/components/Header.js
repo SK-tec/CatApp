de25b748828e7c64ade4bc74279ae5ec1b0fadb9
David Niermann <dc.niermann@gmail.com>
2023-03-11 18:02:48 +0100

    Cards width

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,11 @@
 import logo from "../Images/KittyCatClub_logo.png";
+
 export default function Header() {
   return (
     <>
       <nav className="bg-white px-2 sm:px-4 py-2.5 bg-yellow-600 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
         <div className="container flex flex-wrap items-center justify-between mx-auto">
-          <a href="#" className="flex items-center">
+          <a href="#!" className="flex items-center">
             <img
               src={logo}
               className="h-24 mr-3 sm:h-14"
@@ -48,7 +49,7 @@ export default function Header() {
             <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <a
-                  href=""
+                  href="#!"
                   className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
                   aria-current="page"
                 >
@@ -65,7 +66,7 @@ export default function Header() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="#!"
                   className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Services
@@ -73,7 +74,7 @@ export default function Header() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="#!"
                   className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Contact
