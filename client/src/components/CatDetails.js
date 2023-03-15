7dc1bbcaaab498b05159b562920ba6cf08511895
David Niermann <dc.niermann@gmail.com>
2023-03-15 17:16:02 +0100

    Cat details page (needs styling) and delete functionality

--- client/src/components/CatDetails.js
+++ client/src/components/CatDetails.js
@@ -1,1 +1,61 @@
-export default function CatDetails() {}
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "../axiosInstance";
+
+export default function CatDetails() {
+  const [cat, setCat] = useState([]);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`/api/cats/details/${id}`)
+      .then((res) => setCat(res.data))
+      .catch((e) => console.log(e));
+  }, [id]);
+
+  const handleRemove = () => {
+    axios
+      .delete(`/api/cats/details/${id}`)
+      .then((res) => navigate("/"))
+      .catch((e) => console.log(e));
+  };
+
+  return (
+    <div
+      key={cat._id}
+      className="md:container mx-auto p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5"
+    >
+      <div className="rounded overflow-hidden shadow-xl">
+        <img
+          className="w-full"
+          src={cat.url}
+          alt={cat.breed}
+          style={{
+            width: "100%",
+            height: "400px",
+            objectFit: "cover",
+            objectPosition: "40% 0",
+          }}
+        />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{cat.breed}</div>
+          <p className="text-gray-700 text-base">{cat.temperament}</p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            {cat.life_spanMin} - {cat.life_spanMax} years old
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            from {cat.origin}
+          </span>
+        </div>
+        <div className="px-6 py-4">
+          <button onClick={handleRemove} className="btn btn-blue">
+            Delete Cat
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/components/Cats.js
+++ client/src/components/Cats.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "../axiosInstance";
 
 export default function Cats() {
@@ -24,114 +24,6 @@ export default function Cats() {
     },
     {
       _id: 3,
-      breed: "Bengal",
-      temperament: "Active, cute, sweet, adorable",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
-      life_spanMin: 12,
-      life_spanMax: 18,
-      origin: "India",
-    },
-    {
-      _id: 4,
-      breed: "Bengal",
-      temperament: "Active, cute, sweet, adorable",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
-      life_spanMin: 12,
-      life_spanMax: 18,
-      origin: "India",
-    },
-    {
-      _id: 5,
-      breed: "Bengal",
-      temperament: "Active, cute, sweet, adorable",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
-      life_spanMin: 12,
-      life_spanMax: 18,
-      origin: "India",
-    },
-    {
-      _id: 6,
-      breed: "Bengal",
-      temperament: "Active, cute, sweet, adorable",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
-      life_spanMin: 12,
-      life_spanMax: 18,
-      origin: "India",
-    },
-    {
-      _id: 7,
-      breed: "Bengal",
-      temperament: "Active, cute, sweet, adorable",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
-      life_spanMin: 12,
-      life_spanMax: 18,
-      origin: "India",
-    },
-    {
-      _id: 8,
-      breed: "Bengal",
-      temperament: "Active, cute, sweet, adorable",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
-      life_spanMin: 12,
-      life_spanMax: 18,
-      origin: "India",
-    },
-    {
-      _id: 9,
-      breed: "Bengal",
-      temperament: "Active, cute, sweet, adorable",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
-      life_spanMin: 12,
-      life_spanMax: 18,
-      origin: "India",
-    },
-    {
-      _id: 10,
-      breed: "Bengal",
-      temperament: "Active, cute, sweet, adorable",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
-      life_spanMin: 12,
-      life_spanMax: 18,
-      origin: "India",
-    },
-    {
-      _id: 11,
-      breed: "Bengal",
-      temperament: "Active, cute, sweet, adorable",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
-      life_spanMin: 12,
-      life_spanMax: 18,
-      origin: "India",
-    },
-    {
-      _id: 12,
-      breed: "Bengal",
-      temperament: "Active, cute, sweet, adorable",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
-      life_spanMin: 12,
-      life_spanMax: 18,
-      origin: "India",
-    },
-    {
-      _id: 13,
-      breed: "Bengal",
-      temperament: "Active, cute, sweet, adorable",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
-      life_spanMin: 12,
-      life_spanMax: 18,
-      origin: "India",
-    },
-    {
-      _id: 14,
-      breed: "Bengal",
-      temperament: "Active, cute, sweet, adorable",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
-      life_spanMin: 12,
-      life_spanMax: 18,
-      origin: "India",
-    },
-    {
-      _id: 15,
       breed: "Bengal",
       temperament: "Active, cute, sweet, adorable",
       url: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
@@ -164,21 +56,33 @@ export default function Cats() {
   return (
     <div className="md:container mx-auto p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
       {cats.map((cat) => (
-        <div key={cat._id} className="rounded overflow-hidden shadow-lg">
-          <img className="w-full" src={cat.url} alt={cat.breed} />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">{cat.breed}</div>
-            <p className="text-gray-700 text-base">{cat.temperament}</p>
+        <Link to={`/cat/${cat._id}`} className="title">
+          <div key={cat._id} className="rounded overflow-hidden shadow-xl">
+            <img
+              className="w-full"
+              src={cat.url}
+              alt={cat.breed}
+              style={{
+                width: "100%",
+                height: "400px",
+                objectFit: "cover",
+                objectPosition: "40% 0",
+              }}
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{cat.breed}</div>
+              <p className="text-gray-700 text-base">{cat.temperament}</p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                {cat.life_spanMin} - {cat.life_spanMax} years old
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                from {cat.origin}
+              </span>
+            </div>
           </div>
-          <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              {cat.life_spanMin} - {cat.life_spanMax} years old
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              from {cat.origin}
-            </span>
-          </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
